15bea7590e906bfe192535034c29a713759e4eda
Gustavo <Gustavomarianodelgado@gmail.com>
2019-05-05 02:35:24 -0300

    commit en github

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ client.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('Pong!');
     }
+    if (msg.content === 'hello') {
+        msg.reply('HOLA mundo')
+    }
 });
 
 client.login('NTc0NDQzODA3NjkzODY0OTc5.XM5gaA.i6ev0Rvs9iX-kkWbG6oDWk9IgeI');
