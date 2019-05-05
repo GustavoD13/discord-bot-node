25104cb4273254b0cfaf4c99800b813f80b01de4
Gustavo <Gustavomarianodelgado@gmail.com>
2019-05-05 02:08:50 -0300

    mi primer bot deployment

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { Client } = require('discord.js');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('BOT is ready');
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login('NTc0NDQzODA3NjkzODY0OTc5.XM5gaA.i6ev0Rvs9iX-kkWbG6oDWk9IgeI');
